1fc809689ee6bd437e988c8477afb85e34ebd7d5
eric <eric.malamisura@gmail.com>
2019-10-30 13:08:59 -0400

    ...

--- dist/index.js
+++ dist/index.js
@@ -22837,6 +22837,7 @@ switch (mode) {
             core.info("Skipping Pulumi action altogether...");
             process.exit(0);
         }
+        core.info(github.context);
         branch = github.context.base_ref;
         break;
     default:
--- src/index.ts
+++ src/index.ts
@@ -46,6 +46,7 @@ switch (mode) {
       core.info("Skipping Pulumi action altogether...");
       process.exit(0);
     }
+    core.info(github.context);
     branch = github.context.base_ref;
     break;
   default:
